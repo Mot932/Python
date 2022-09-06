92571e1747905d6b067d5d3180cb24a0d7ef6e9f
Mot932 <112969476+Mot932@users.noreply.github.com>
2022-09-06 19:42:03 +0300

    втора версия

--- ya3rik.py
+++ ya3rik.py
@@ -1,5 +1,7 @@
 age = input ("сколько тебе лет,пупсик?")
-if int(age) >= 18:
+if int(age) >= 18 and int(age) <=20:
     print("иди в кроватку пупсик!")
+elif int(age) <18:
+    print("мелкий")
 else:
     print("в кроватку к отчимам быстро пупсик!")
