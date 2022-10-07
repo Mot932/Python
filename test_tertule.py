37de761910eb986624a25300acccaacffd712f72
DEB <112969476+Mot932@users.noreply.github.com>
2022-10-07 19:46:35 +0300

    Add files via upload

---
+++ test_tertule.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.color("red")
+turtle.shape("turtle")
+
+
+for i in range(4):
+    turtle.fd(100)
+    turtle.left(90)
+
+turtle.left(90)
+turtle.fd(100)
+turtle.right(45)
+turtle.fd(80)
+turtle.left(45)
+turtle.left(217)
+turtle.fd(80)
+
+turtle.done()
