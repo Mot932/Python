6ab0a6fcb1389cc3b42ce3a81d6bd9f49fb99351
DEB <112969476+Mot932@users.noreply.github.com>
2022-10-14 19:41:53 +0300

    Add files via upload

---
+++ yt.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+
+start_radius = 50
+for i in range(3):
+    t.circle(start_radius)
+    start_radius = start_radius / 2
+    if start_radius < 5:
+        break
+    inpet("wewewewewewe")
+
+
+t.done()
